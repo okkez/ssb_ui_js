729289c2ebe1d69b2e896962238f959dadf217cf
okkez <okkez000@gmail.com>
2009-02-28 00:46:02 +0900

    add jquery.json plugin

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,11 @@
 // Stack Stock Books
 var Ssb = {
-
+  DEBUG: true,
+  log: function(arg){
+    if (Ssb.DEBUG) {
+      console.log(arg);
+    }
+  }
 };
 
 Ssb.Util = {
@@ -23,7 +28,7 @@ Ssb.Util = {
   url: function(template, options, params){
     var result = this.t(template, options);
     result = this.append_query_parameters(result, params);
-    console.log(result);
+    Ssb.log(result);
     return result;
   }
 };
@@ -42,6 +47,15 @@ Ssb.API.Book = {
   },
   find_stocks_by_user_and_state: function(options, params, callback){
     $.getJSON(Ssb.Util.url(Ssb.API.site+"user/{user_id_type}/{user_id}/stocks/{state}.json", options, params), callback);
+  },
+  create_or_update: function(book, callback) {
+    if (Ssb.API.token == null || Ssb.API.token == ""){
+      return;
+    }
+    // book = { asin:1234567890, date:'yyyy-mm-dd', state:'unread', public:true }
+    var body = $.toJSON([book]);
+    Ssb.log(body);
+    $.post(Ssb.Util.url(Ssb.API.site+"{user_name}/{token}/stocks/update.1", { user_name: Ssb.API.user_name, token: Ssb.API.token }), { request: body }, callback);
   }
 };
 
@@ -59,7 +73,7 @@ Ssb.View = {
 		$("<div>").attr("id", "state_tabs-3").addClass("books"),
 		$("<div>").attr("id", "state_tabs-4").addClass("books")));
     $("div#state_tabs > ul").tabs({
-      selected: 0,
+      selected: null,
       cache: true
     }).bind("tabsselect", function(event, original_event, ui){
       // ui.tab 選択されたタブを表す
@@ -75,6 +89,28 @@ Ssb.View = {
       "state_tabs-3": "wish",
       "state_tabs-4": "read"
     };
+    var update_state_links = function(stock){
+      var states_table = {
+	"reading": ["unread", "wish", "read"],
+	"unread": ["reading", "wish", "read"],
+	"wish": ["reading", "unread", "read"],
+	"read": ["reading", "unread", "wish"]
+      };
+      var update_state_link = function(state){
+	return $("<a>").attr("href", "#")
+	  .text(state+" ").click(function(){
+	    Ssb.API.Book.create_or_update({
+	      asin: stock.book.isbn10,
+	      date: null,
+	      state: state,
+	      "public": true
+	    }, function(data, status){ Ssb.log(data.message); });
+	  });
+      };
+      return $.map(states_table[stock.state], function(v, index){
+	return update_state_link(v);
+      });
+    };
     var state = states[panel.attr("id")];
     Ssb.API.Book.find_stocks_by_user_and_state(
       { user_id_type: "name", user_id: Ssb.API.user_name, state: state },
@@ -83,12 +119,15 @@ Ssb.View = {
 	if (data.success) {
 	  $("#errorExplanation").empty();
 	  panel.append($("<ul>"));
-	  $.each(data.response.stocks, function(index, stock){
+	  $.each(data.response.stocks, function(index, stock) {
 	    $("ul", panel)
-		     .append($("<li>").addClass("stock")
+		     .append($("<li>").attr("id", stock.stock_id.toString()).addClass("stock")
 			     .append($("<a>").text(stock.book.title).attr("href", stock.book.uri),
 				     $("<br>"),
 				     $("<span>").text("Update State : ")));
+            $.each(update_state_links(stock), function(index, alink) {
+	      $("ul > li#" + stock.stock_id  + " > span", panel).append(alink);
+	    });
 	  });
 	  $("li.stock:nth-child(odd)").css("background-color", "#FFFCD0");
 	  $("li.stock:nth-child(even)").css("background-color", "#DEF1FD");
